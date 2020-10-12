ebef4503b636b22bea7a007d4dd4c87033941174
Leon <t7oele00@students.oamk.fi>
2020-10-12 13:57:54 +0300

    Trying Assets, buttons, stacks, containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
-import './random_words.dart';
+import 'package:flutter/rendering.dart';
+// import './random_words.dart';
 
 void main() => runApp(MyApp());
+
+/* class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.pink[200]), home: RandomWords());
+  }
+} */
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: RandomWords());
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Totally legit Blizzard Armory'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent[200],
+        ),
+        body: new Stack(children: <Widget>[
+          new Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/img.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          new Center(
+            child: new RaisedButton(
+              onPressed: () {/*..*/},
+              child: Text('Connect your Blizzard Account'),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
