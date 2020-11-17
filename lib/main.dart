813d4978212d75eaf03cecc2ee0bf5926abd717d
Leon <t7oele00@students.oamk.fi>
2020-11-17 15:12:52 +0200

    2.0 Fixed main routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 import './home.dart';
-import './realms.dart';
-import './raideriopost.dart';
-import './database/characterList.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/character_bloc.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(home: Home());
-//   }
-// }
 
 void main() => runApp(MyApp());
 
