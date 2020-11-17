77913c2be852ab1597aff3c3ad38495ead0c8d03
Leon <t7oele00@students.oamk.fi>
2020-11-17 13:12:38 +0200

    Open Database

---
+++ lib/databaseService.dart
@@ -0,0 +1,45 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseService {
+  static const String TABLE_CHARACTER = "character";
+  static const String COLUMN_ID = "id";
+  static const String COLUMN_NAME = "name";
+  static const String COLUMN_RACE = "race";
+  static const String COLUMN_LEVEL = "level";
+  static const String COLUMN_FACTION = "faction";
+
+  DatabaseService._();
+  static final DatabaseService db = DatabaseService._();
+
+  Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+
+    _database = await createDatabase();
+
+    return _database;
+  }
+
+  Future<Database> createDatabase() async {
+    String dbPath = await getDatabasesPath();
+
+    return await openDatabase(join(dbPath, 'characterDb.db'), version: 1,
+        onCreate: (Database database, int version) async {
+      print("Creating character table");
+
+      await database.execute(
+        "CREATE TABLE $TABLE_CHARACTER ("
+        "$COLUMN_ID INTEGER PRIMARY KEY,"
+        "$COLUMN_NAME TEXT,"
+        "$COLUMN_RACE TEXT,"
+        "$COLUMN_LEVEL TEXT,"
+        "$COLUMN_FACTION TEXT,"
+        ")",
+      );
+    });
+  }
+}
